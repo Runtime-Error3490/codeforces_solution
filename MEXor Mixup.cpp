a140dfed450bce5e92bc46bf4f6103632ca932c4
Shaleen Shukla <93307147+Runtime-Error3490@users.noreply.github.com>
2023-02-11 00:17:00 +0530

    Create MEXor Mixup.cpp
    https://codeforces.com/problemset/problem/1567/B

---
+++ MEXor Mixup.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int            long long int
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define endl           "\n"
+#define sz(x)          ((int) x.size())
+#define all(p)         p.begin(), p.end()
+#define double         long double
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
+  int n=1e6; 
+ vector<bool> is_prime(n+1, true);
+ 
+ void Prime(){
+is_prime[0] = is_prime[1] = false;
+for (int i = 2; i <= n; i++) {
+	if (is_prime[i] && (long long)i * i <= n) {
+		for (int j = i * i; j <= n; j += i)
+			is_prime[j] = false;
+	}
+}
+ }
+inline int power(int a, int b)
+{
+	int x = 1;
+	while (b)
+	{
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+const int N = 200005;
+int computeXOR(int n)
+{
+   
+  // If n is a multiple of 4
+  if (n % 4 == 0)
+    return n;
+ 
+  // If n%4 gives remainder 1
+  if (n % 4 == 1)
+    return 1;
+ 
+  // If n%4 gives remainder 2
+  if (n % 4 == 2)
+    return n + 1;
+ 
+  // If n%4 gives remainder 3
+  return 0;
+}
+void solve() {
+	int a,b;
+	cin>>a>>b;
+	int x=0;
+	x=computeXOR(a-1);
+	int ans=0;
+	ans=ans+a;
+	
+	if(x==b)
+	{
+		cout<<ans<<endl;
+		return;
+	}
+	if((x^b)==a)
+	{
+		ans=ans+2;
+	}
+	else
+	{
+		ans=ans+1;
+	}
+	cout<<ans<<endl;
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt",  "r",  stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	clock_t z = clock();
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+
+	return 0;
+}
